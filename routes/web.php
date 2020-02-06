99313abe55092afc0cf39f6462af9667e5986f30
SirJeany <jmvanriet1@gmail.com>
2020-02-06 13:54:23 +0200

    Route Wildcards

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// using request data from view
 Route::get('/test', function () {
     $name = request('name');
 
     return view('test', [
         'name' => $name
+    ]);
+});
+
+//Route wildcards:
+Route::get('/post/{posts}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hey there, this is the first one!',
+        'my-second-post' => 'Hey there, Number 2!',
+    ];
+
+    if(! array_key_exists($post, $posts))
+        abort(404, 'Sorry that post was not found, try: my-first-post');
+
+    return view('post',[
+        'post' => $posts[$post]
     ]);
 });
