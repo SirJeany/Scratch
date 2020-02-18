b3e564bc6faaa0d7c6fde822d3ff9cb6f92b094a
SirJeany <jmvanriet1@gmail.com>
2020-02-18 19:07:38 +0200

    Model:Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PostsController{
 
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        // $post = \DB::table('posts')->where('slug', $slug)->first();
+        // using DB query --^ vs using eloquent:
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         return view('post', [
             'post' => $post
